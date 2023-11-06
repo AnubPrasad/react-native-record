2c958c8b362a1773f11dd95d23df1820908db023
Shiyam <shiyam@A-iMac.local>
2023-11-06 17:33:36 +0530

    first commit

---
+++ data/questions.js
@@ -0,0 +1,214 @@
+
+export default [
+    {
+        question: "If January 1, 1996, was Monday, What day of the week was January 1, 1997?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"Thursday",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"Friday",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"Wednesday",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"Sunday",
+            },
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "A train moving at speed of 80 km/hr crosses a pole in 7 seconds. Find the length of the train? ",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"175",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"170",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"150",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"165",
+            },
+        ],
+        correctAnswerIndex: 0,
+
+    },
+    {
+        question: "A pipe can fill a tank in 6 hours and another pipe can empty the tank in 12 hours. If both the pipes are opened at the same time, the tank can be filled in ?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"10",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"12",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"14",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"16",
+            },
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "what is the square root of 169",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"20",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"23",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"13",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"23",
+            },
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "This is the girl ________ father is a scientist.",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"who",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"whom",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"whose",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"where",
+            },
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "________ of the students were making a great noise.",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"Anyone",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"Somebody",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"Many",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"None of these",
+            },
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "My sister is a __________.",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"bacheloress",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"bachelor",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"spinster",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"None of these",
+            },
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "A lady says to a man, Your father is the father in law of my father's only child. How is the man related to the lady?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"wife",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"daughter",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"Husband",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"father",
+            },
+        ],
+        correctAnswerIndex: 2
+    }
+   
+
+]
